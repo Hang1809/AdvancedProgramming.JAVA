4dcd732139c7e69dcebd33ecea9c0d321579c684
Hang1809 <hangdinhphankhanh@gmail.com>
2022-04-01 15:20:11 +0700

    TimGiaTriLonNhatMaTran

---
+++ FindMaxValue/src/FindMaxValueMatrix.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class FindMaxValueMatrix {
+
+    public static void main(String[] args) {
+
+        int row, col;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Input the number row of Matrix: ");
+        row = scanner.nextInt();
+        System.out.println("Input the number colum of Matrix: ");
+        col = scanner.nextInt();
+
+
+        int Array[][] = new int[row][col];
+        System.out.println("Input value of Index in Matrix: ");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(" Array[" + i + "][" + j + "] = ");
+                Array[i][j] = scanner.nextInt();
+            }
+        }
+
+
+        int max = Array[0][0];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (max < Array[i][j]) {
+                    max = Array[i][j];
+                }
+            }
+        }
+
+        System.out.println("The largest property value in Matrix is: " + max);
+
+    }
+}
+
